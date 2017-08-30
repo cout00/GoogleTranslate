ea2a1285a9a66191b9166cd45ff0386780c8ec79
aizik <aizik@DESKTOP-DCAJ653>
2017-08-30 11:40:13 +0300

    Start Version

---
+++ GoogleTranslate.Tests/ParserTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace GoogleTranslate.Tests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+
+
+
+    }
+}
